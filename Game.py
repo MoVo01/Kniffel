996db3607f854505f4d8b2082f9505eee0d75300
MoVo01 <moritzvogel24@gmail.com>
2020-08-20 18:43:46 +0200

    add unittest

---
+++ Game.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 20 16:29:53 2020
+
+@author: morit
+"""
+
+
+class Game:
+    pass
---
+++ Unittests.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 20 16:31:01 2020
+
+@author: morit
+"""
+
+
