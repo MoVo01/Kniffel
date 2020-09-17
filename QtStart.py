9663c555d557b2f5f20661c2b99e7e5e855debda
odysseus1904 <69589665+odysseus1904@users.noreply.github.com>
2020-09-17 22:12:58 +0200

    Create QtStart.py

---
+++ QtStart.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 17 21:56:15 2020
+
+@author: luisw
+"""
+
+
+import sys
+from PyQt5 import QtCore, QtWidgets, uic
+
+
+Ui_MainWindow, WindowBaseClass = uic.loadUiType("GUI.ui")
+
+class MyDialog(WindowBaseClass, Ui_MainWindow):
+    def __init__(self, parent=None):
+        WindowBaseClass.__init__(self, parent)
+        Ui_MainWindow.__init__(self)
+        self.setupUi(self)
+    
+if __name__ == "__main__":
+    # In Spyder kann nur eine Qt-Applikation laufen und sie werden nicht anschliessend geloescht
+    if QtCore.QCoreApplication.instance() is not None:
+        app = QtCore.QCoreApplication.instance()
+    else:
+        app = QtWidgets.QApplication(sys.argv)
+
+    dialog = MyDialog()
+    dialog.show()
+    sys.exit(app.exec_())
