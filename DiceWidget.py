d41f538361c1fd615959d1bfe2879a369294c033
odysseus1904 <69589665+odysseus1904@users.noreply.github.com>
2020-10-16 15:57:51 +0200

    Update DiceWidget.py

--- DiceWidget.py
+++ DiceWidget.py
@@ -6,37 +6,88 @@ Created on Thu Sep 17 21:55:27 2020
 """
 
 
-from PyQt5 import QtCore, QtGui, QtWidgets
+import os
+from DieRoll import DiceRoll
+from PyQt5 import QtCore
+from PyQt5.Qt import Qt
+from PyQt5.QtGui import QPainter, QPixmap, QCursor, QBrush
+from PyQt5.QtWidgets import QWidget, QMessageBox, QApplication
 
-class DiceWidget(QtWidgets.QWidget):
+class DiceWidget(QWidget):
     
     def __init__(self, parent=None):
         super().__init__(parent)   
         # generate mouse move events (see below)
         #self.setMouseTracking(True)
         
+        self.dice_width = 20
+        
+        Eins = QPixmap(os.path.join("Würfel", "Eins"))
+        Zwei = QPixmap(os.path.join("Würfel", "Zwei"))
+        Drei = QPixmap(os.path.join("Würfel", "Drei"))
+        Vier = QPixmap(os.path.join("Würfel", "Vier"))
+        Fuenf = QPixmap(os.path.join("Würfel", "Fünf"))
+        Sechs = QPixmap(os.path.join("Würfel", "Sechs"))
+        
+        
+        self.eins= Eins
+        #self.eins = Eins.scaledToWidth(self.dice_width, Qt.SmoothTransformation)
+        self.zwei = Zwei.scaledToWidth(self.dice_width, Qt.SmoothTransformation)
+        self.drei = Drei.scaledToWidth(self.dice_width, Qt.SmoothTransformation)
+        self.vier = Vier.scaledToWidth(self.dice_width, Qt.SmoothTransformation)
+        self.fuenf = Fuenf.scaledToWidth(self.dice_width, Qt.SmoothTransformation)
+        self.sechs = Sechs.scaledToWidth(self.dice_width, Qt.SmoothTransformation)
+        
     def paintEvent(self, event):
         super().paintEvent(event)
         
-        painter = QtGui.QPainter(self)
-        painter.setRenderHints(QtGui.QPainter.Antialiasing)
-        
-        painter.setPen(QtCore.Qt.NoPen) # no boundary
-        square_brush = QtGui.QBrush(QtCore.Qt.black)
-        painter.setBrush(square_brush)
-        
-        # placement and size relative to window size
-        M = [self.width() / 4, self.height() / 4]
-        r = min(self.width(), self.height()) / 4
-        rect = QtCore.QRectF(M[0]-r, M[1]-r, 2*r, 2*r)
-        
-        painter.drawRect(rect)
-        
-        painter.setBackground(QtCore.Qt.green)
-        
-        diceMap = QtGui.QPixmap(self).loadFromData()
+        painter = QPainter(self)
         
         
+        painter.setPen(Qt.NoPen)
+        painter.setBrush(Qt.darkGreen)
+        painter.drawRect(event.rect())
+        
+        
+        newRoll = DiceRoll()
+        
+        #newRoll.pick(1)
+        #newRoll.pick(3)
+        painter.drawPixmap(100, 100, self.eins)
+        
+        for i in range (0,len(newRoll.free_dice)):
+            if newRoll.free_dice[i] == 1:
+                painter.drawPixmap(self.width() * ((i+1) / (len(newRoll.free_dice)+1)),2 * self.height() / 3, self.eins)
+            elif newRoll.free_dice[i] == 2:
+                painter.drawPixmap(self.width() * ((i+1) / (len(newRoll.free_dice)+1)),2 * self.height() / 3, self.zwei)
+            elif newRoll.free_dice[i] == 3:
+                painter.drawPixmap(self.width() * ((i+1) / (len(newRoll.free_dice)+1)),2 * self.height() / 3, self.drei)
+            elif newRoll.free_dice[i] == 4:
+                painter.drawPixmap(self.width() * ((i+1) / (len(newRoll.free_dice)+1)),2 * self.height() / 3, self.vier)
+            elif newRoll.free_dice[i] == 5:
+                painter.drawPixmap(self.width() * ((i+1) / (len(newRoll.free_dice)+1)),2 * self.height() / 3, self.fuenf)
+            elif newRoll.free_dice[i] == 6:
+                painter.drawPixmap(self.width() * ((i+1) / (len(newRoll.free_dice)+1)),2 * self.height() / 3, self.sechs)
+            else:
+                print("SOmethings wrong")
+          
+        for i in range (0,len(newRoll.picked_dice)):
+            if newRoll.picked_dice[i] == 1:
+                painter.drawPixmap(self.width() * ((i+1) / (len(newRoll.picked_dice)+1)),self.height() / 3, self.eins)
+            elif newRoll.picked_dice[i] == 2:
+                painter.drawPixmap(self.width() * ((i+1) / (len(newRoll.picked_dice)+1)),self.height() / 3, self.zwei)
+            elif newRoll.picked_dice[i] == 3:
+                painter.drawPixmap(self.width() * ((i+1) / (len(newRoll.picked_dice)+1)),self.height() / 3, self.drei)
+            elif newRoll.picked_dice[i] == 4:
+                painter.drawPixmap(self.width() * ((i+1) / (len(newRoll.picked_dice)+1)),self.height() / 3, self.vier)
+            elif newRoll.picked_dice[i] == 5:
+                painter.drawPixmap(self.width() * ((i+1) / (len(newRoll.picked_dice)+1)),self.height() / 3, self.fuenf)
+            elif newRoll.picked_dice[i] == 6:
+                painter.drawPixmap(self.width() * ((i+1) / (len(newRoll.picked_dice)+1)),self.height() / 3, self.sechs)
+            else:
+                print("SOmethings wrong")
+                
+            
         
 if __name__ == "__main__":
     import sys
@@ -44,9 +95,10 @@ if __name__ == "__main__":
     if QtCore.QCoreApplication.instance() is not None:
         app = QtCore.QCoreApplication.instance()
     else:
-        app = QtWidgets.QApplication(sys.argv)
+        app = QApplication(sys.argv)
         
     dice = DiceWidget()
-    dice.resize(400,200)
+    dice.resize(800,600)
     dice.show()
-    sys.exit(app.exec_())    
+    sys.exit(app.exec_())
+
